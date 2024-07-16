a4ff8aff96a3d1d1b51cbfa77bc9f4a9935f5de1
JakubSchwenkbeck <jakub@schwenkbeck.de>
2024-07-16 20:56:06 +0200

    Tested the assembler level function and it worked!

--- src/bin/VirtualMachine.rs
+++ src/bin/VirtualMachine.rs
@@ -1,12 +1,20 @@
 // Virtual Machine Binary 
 use rust_projects::vm::Machine;
+use rust_projects::instruction_set::*;
+use rust_projects::memory::{LinMem,Addressable};
+use rust_projects::vm::Register;
+
 
 pub fn main()-> Result<(), & 'static str> {
     let mut virtualm = Machine::new();
     //println!("reg {}",virtualm.registers[Register::PC]);
-    virtualm.step()?;
-    virtualm.step()?;
-    virtualm.step()?;
+    add_immediate(&mut virtualm, 1, 1, 15);
+    let val =virtualm.memory.read(1);
+    let mut disp = 1;
+    if val.is_some(){
+         disp = val.unwrap();
+    }
+    println!("{disp}");
     virtualm.step()
 
 
